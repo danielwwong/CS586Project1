bc0d20454c42c10766be143ad9c00759afa05c42
danielwwong <vipyellowbee@gmail.com>
2017-09-25 09:52:14 -0400

    first 13671 result

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 
 with open('gameID.json') as src:
     app_id = json.load(src)
@@ -8,16 +9,19 @@ with open('game.json', 'w') as init:
     json.dump([], init)
 init.close()
 
-count = len(app_id["applist"]["apps"]["app"])
-for i in range(0, count):
-    url_id = str(app_id["applist"]["apps"]["app"][i]["appid"])
-    r = requests.get('http://store.steampowered.com/api/appdetails?appids=' + url_id + '&cc=us&filters=price_overview')
-    output_data = r.json()
-    with open('game.json', 'r') as read:
-        appe = json.load(read)
-    appe.append(output_data)
-    with open('game.json', 'w') as out:
-        json.dump(appe, out)
+count = len(app_id["applist"]["apps"]["app"])#48213
+
+for i in range(1, 81):#242
+    for j in range((200 * i) - 200, 200 * i):
+        url_id = str(app_id["applist"]["apps"]["app"][j]["appid"])
+        r = requests.get('http://store.steampowered.com/api/appdetails?appids=' + url_id + '&cc=us&filters=price_overview')
+        output_data = r.json()
+        with open('game.json', 'r') as read:
+            appe = json.load(read)
+        appe.append(output_data)
+        with open('game.json', 'w') as out:
+            json.dump(appe, out)
+    time.sleep(305)
 
 src.close()
 read.close()
