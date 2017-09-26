da7e192a1558ae0c20aefa5466ae07141e0daeed
danielwwong <vipyellowbee@gmail.com>
2017-09-26 19:40:02 -0400

    Better Code

--- game.py
+++ game.py
@@ -11,17 +11,23 @@ init.close()
 
 count = len(app_id["applist"]["apps"]["app"])#48213
 
-for i in range(1, 81):#242
-    for j in range((200 * i) - 200, 200 * i):
-        url_id = str(app_id["applist"]["apps"]["app"][j]["appid"])
-        r = requests.get('http://store.steampowered.com/api/appdetails?appids=' + url_id + '&cc=us&filters=price_overview')
-        output_data = r.json()
-        with open('game.json', 'r') as read:
-            appe = json.load(read)
-        appe.append(output_data)
-        with open('game.json', 'w') as out:
-            json.dump(appe, out)
-    time.sleep(305)
+for i in range(0, 48213):
+    url_id = str(app_id["applist"]["apps"]["app"][i]["appid"])
+    r = requests.get('http://store.steampowered.com/api/appdetails?appids=' + url_id + '&cc=us&filters=price_overview')
+    output_data = r.json()
+    with open('game.json', 'r') as read:
+        appe = json.load(read)
+    appe.append(output_data)
+    with open('game.json', 'w') as out:
+        json.dump(appe, out)
+    if(output_data is None):
+        print(i)
+        break
+    print(i)
+    print(output_data)
+    time.sleep(1.6)
+    if (i % 200 == 0):
+        time.sleep(10)
 
 src.close()
 read.close()
